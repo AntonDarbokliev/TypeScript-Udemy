18bb36c0814bd3509b886e6331f1f45f46223eb5
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-20 15:13:15 +0200

    enum

--- Basics/app.ts
+++ Basics/app.ts
@@ -1,19 +1,29 @@
-const person
- : {
-    name : string;
-    age : number;
-    hobbies: string[];
-    roles: [number,string]; //tuple
-} 
-= {
-    name: 'Anton',
-    age: 18,
-    hobbies : ['Skiing','Football'],
-    roles : [2,'admin']
-}
+// const person
+//  : {
+//     name : string;
+//     age : number;
+//     hobbies: string[];
+//     roles: [number,string]; //tuple
+// } 
+// = {
+//     name: 'Anton',
+//     age: 18,
+//     hobbies : ['Skiing','Football'],
+//     roles : [2,'admin']
+// }
 
 // person.roles.push('a') //not supported by ts
 // person.roles = []
+
+enum Role {ADMIN, GUEST, READ_ONLY}  //Enum - used to pair keys with human readable values for easier usage
+
+
+const person = {
+    name: 'Anton',
+    age: 18,
+    hobbies : ['Skiing','Football'],
+    role : Role.ADMIN
+}
 
 for(const hobbie of person.hobbies){
     console.log(hobbie.toLowerCase());
