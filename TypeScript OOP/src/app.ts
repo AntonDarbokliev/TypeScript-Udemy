b7356f73719f7f4fbaac15086caceb9a057be030
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-21 20:54:03 +0200

    class and instances

--- TypeScript OOP/src/app.ts
+++ TypeScript OOP/src/app.ts
@@ -1,1 +1,21 @@
-// Code goes here!
+class Department {
+    name: string;
+    location:string
+
+    constructor(n: string,l:string) {
+        this.name = n;
+        this.location = l
+    }
+
+    describe(this:Department) {
+        console.log(`This is the ${this.name} department`);
+    }
+}
+
+const accounting = new Department("Accounting",'Brooklyn');
+
+const accountingCopy = {name: 'Accounting', location:'Copy location',describe: accounting.describe };
+
+accounting.describe();
+
+accountingCopy.describe();
