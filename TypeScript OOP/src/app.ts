9e60ed517f732b75cc6a12b4b41abf626d968774
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-21 21:07:56 +0200

    Private methos/properties

--- TypeScript OOP/src/app.ts
+++ TypeScript OOP/src/app.ts
@@ -1,21 +1,42 @@
 class Department {
     name: string;
-    location:string
+    private employees : string[];
+    location: string;
 
-    constructor(n: string,l:string) {
+    constructor(n: string, l: string,) {
         this.name = n;
-        this.location = l
+        this.location = l;
+        this.employees = []
     }
 
-    describe(this:Department) {
+    describe(this: Department) {
         console.log(`This is the ${this.name} department`);
+    }
+
+    addEmployee(employee:string){
+        this.employees.push(employee)
+    }
+
+    printEmployees(){
+        console.log('Employees: ' + this.employees.join(', '));
     }
 }
 
-const accounting = new Department("Accounting",'Brooklyn');
+const accounting = new Department("Accounting", "Brooklyn");
 
-const accountingCopy = {name: 'Accounting', location:'Copy location',describe: accounting.describe };
+accounting.addEmployee('Anton')
+accounting.addEmployee('Max')
+// accounting.employees.push('Ben') 
+// Not a good idea to have two ways of adding an employee, hence why we're making it a private propertie
+
+accounting.printEmployees()
 
 accounting.describe();
 
-accountingCopy.describe();
+// const accountingCopy = {
+//     name: "Accounting",
+//     location: "Copy location",
+//     describe: accounting.describe,
+// };
+
+// accountingCopy.describe();
