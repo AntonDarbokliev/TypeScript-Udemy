922c289cf3c35d2530e3ac470f723fa7d527754c
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-23 22:40:29 +0200

    different type guards

--- TypeScript Advanced Types/src/app.ts
+++ TypeScript Advanced Types/src/app.ts
@@ -1,19 +1,77 @@
 type Employee = {
-    name: string,
-    age: number,
-}
+    name: string;
+    age: number;
+};
 
 type Admin = {
-    name: string,
-    privileges: string[]
-}
+    name: string;
+    privileges: string[];
+};
 
-type ElevatedEmployee = Employee & Admin
+type ElevatedEmployee = Employee & Admin;
 
 let employee: ElevatedEmployee;
 
 employee = {
-    name : 'Anton',
+    name: "Anton",
     age: 18,
-    privileges : ['User', 'Admin']
+    privileges: ["User", "Admin"],
+};
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInfo(emp: UnknownEmployee) {
+    console.log("Name:  " + emp.name);
+    if ("privileges" in emp) {
+        console.log("Privileges:  " + emp.privileges);
+    }
 }
+
+const emp: UnknownEmployee = {
+    name: "Anton",
+    age: 18,
+};
+
+const adminEmp: UnknownEmployee = {
+    name:'Bob',
+    privileges : ['Admin']
+}
+
+// printEmployeeInfo(emp)
+// printEmployeeInfo(adminEmp)
+
+class Vehicle{
+    drive(){
+        console.log('Currently driving...');
+    }
+}
+
+class Truck extends Vehicle {
+    load(amount:number){
+        console.log('Loading ' + amount + 'kg');
+    }
+}
+
+class Boat extends Vehicle {
+    sail(){
+        console.log('Currently sailing');
+    }
+}
+
+type VehicleType = Truck | Boat
+
+function useVehicle(vehicle: VehicleType){
+    vehicle.drive();
+    if(vehicle instanceof Boat){
+        vehicle.sail()
+    }else if(vehicle instanceof Truck){
+        vehicle.load(200);
+    }
+}
+
+const v1 = new Truck()
+const v2 = new Boat()
+ 
+useVehicle(v1)
+useVehicle(v2)
+
