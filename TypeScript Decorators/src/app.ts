cdd2f0a769970afc79aac7489e9a49c1bdf446c0
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-26 22:48:38 +0200

    change the class using a decorator

--- TypeScript Decorators/src/app.ts
+++ TypeScript Decorators/src/app.ts
@@ -1,67 +1,70 @@
-// function Logger(elementId:string){
-//     return function (constructor:any){
-//     const element = document.getElementById(elementId)
-//     const p = new constructor()
-//         if(element){
-//             element.textContent = p.name
-//         }    
-//     }
-// }
+function WithTemplate(elementId:string){
+    return function <T extends {new(...args:any[]):{name:string}}> (originalConstructor:T){
+        return class extends originalConstructor{
+            constructor(..._:any[]){
+                super()
+                const element = document.getElementById(elementId)
+                    if(element){
+                        element.textContent = this.name
+                    }
+            }
+        }
+    }
+}
 
+@WithTemplate('app')
+class Person {
+    name = 'Max'
 
-// @Logger('app')
-// class Person {
-//     name = 'Max'
+    constructor(){
+        console.log('Creating person object...');
+    }
+}
 
-//     constructor(){
-//         console.log('Creating person object...');
-//     }
-// }
-
-// const person1 = new Person()
+const person1 = new Person()
 
 //  ---
 
 
-function PropertieDecorator(target:any,propertyName: string | Symbol){
-    console.log('Property decorator');
-    console.log(target, propertyName);
-}
+// function PropertieDecorator(target:any,propertyName: string | Symbol){
+//     console.log('Property decorator');
+//     console.log(target, propertyName);
+// }
 
-function AccessorDecorator(target:any,name:string,descriptor: PropertyDescriptor){
-    console.log('Accessor Decorator');
-    console.log(target,name,descriptor);
-}
+// function AccessorDecorator(target:any,name:string,descriptor: PropertyDescriptor){
+//     console.log('Accessor Decorator');
+//     console.log(target,name,descriptor);
+// }
 
-function methodDecorator(target:any,name: string,descriptor: PropertyDescriptor ){
-    console.log('Method Decorator');
-    console.log(target,name,descriptor);
-}
+// function methodDecorator(target:any,name: string,descriptor: PropertyDescriptor ){
+//     console.log('Method Decorator');
+//     console.log(target,name,descriptor);
+// }
 
-function propertieDescriptor(target:any,name:string,position:number){
-    console.log('Propertie Decorator');
-    console.log(target,name,position);
+// function propertieDescriptor(target:any,name:string,position:number){
+//     console.log('Propertie Decorator');
+//     console.log(target,name,position);
 
-}
+// }
 
-class Product {
-    @PropertieDecorator
-    title:string;
-    _price:number;
+// class Product {
+//     @PropertieDecorator
+//     title:string;
+//     _price:number;
 
-    @AccessorDecorator
-    set price (value:number){
-        this._price = value
-    }
+//     @AccessorDecorator
+//     set price (value:number){
+//         this._price = value
+//     }
     
-    constructor(t:string,p:number){
-        this.title = t
-        this._price = p
-    }
+//     constructor(t:string,p:number){
+//         this.title = t
+//         this._price = p
+//     }
 
-    @methodDecorator
-    logPrice(@propertieDescriptor currency:string){
-        console.log(this._price + currency);
-    }
+//     @methodDecorator
+//     logPrice(@propertieDescriptor currency:string){
+//         console.log(this._price + currency);
+//     }
 
-}
+// }
