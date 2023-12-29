ee425c28b972aaa013c024e555ddffcf5cb681e7
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-29 22:29:36 +0200

    use a class validator library

--- TypeScript Third Party Libraries/src/app.ts
+++ TypeScript Third Party Libraries/src/app.ts
@@ -1,33 +1,53 @@
-import _ from 'lodash'
+// import _ from 'lodash'
 import 'reflect-metadata';
-import { plainToClass } from 'class-transformer';
+// import { plainToClass } from 'class-transformer';
 
+import {IsNotEmpty,validate,IsNumber, IsPositive} from 'class-validator'
 
 class Product{
-    constructor(public title:string,public price:number){
+
+    @IsNotEmpty()
+    title:string
+
+    @IsPositive()
+    @IsNumber()
+    price:number
+
+    constructor(title:string, price:number){
         this.title = title
         this.price = price
+        
+        validate(this).then(err => {
+            if(err.length > 0){
+                console.log('ERRORS FOUND');
+                console.log(err);
+            }
+            
+        })
     }
 
     getProductInformation(){
-        console.log([this.title,this.price]);
-        
+        console.log([this.title,this.price]);   
     }
 
 }
 
-const products = [
-    {title:'LOL', price:19.99},
-    {title:'Harry Potter', price:29.99},
-]
+// const products = [
+//     {title:'LOL', price:19.99},
+//     {title:'Harry Potter', price:29.99},
+// ]
 
 // const loadedProducts = products.map(x => new Product(x.title,x.price)) 
 
 
 
-const loadedProducts = plainToClass(Product,products)
+// const loadedProducts = plainToClass(Product,products)
 
-for(const product of loadedProducts){
-    product.getProductInformation()
-}
+// for(const product of loadedProducts){
+//     product.getProductInformation()
+// }
 // console.log(_.shuffle([1,2,3]));
+
+const product = new Product('',19.99)
+console.log(product);
+
