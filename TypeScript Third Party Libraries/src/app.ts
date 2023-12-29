5bccdcd60ba2dd222627af15ae67090344cbfbfb
Anton Darbokliev <georgedarbokliev@gmail.com>
2023-12-29 22:11:01 +0200

    using class transfromer library

--- TypeScript Third Party Libraries/src/app.ts
+++ TypeScript Third Party Libraries/src/app.ts
@@ -1,3 +1,33 @@
 import _ from 'lodash'
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-console.log(_.shuffle([1,2,3]));
+
+class Product{
+    constructor(public title:string,public price:number){
+        this.title = title
+        this.price = price
+    }
+
+    getProductInformation(){
+        console.log([this.title,this.price]);
+        
+    }
+
+}
+
+const products = [
+    {title:'LOL', price:19.99},
+    {title:'Harry Potter', price:29.99},
+]
+
+// const loadedProducts = products.map(x => new Product(x.title,x.price)) 
+
+
+
+const loadedProducts = plainToClass(Product,products)
+
+for(const product of loadedProducts){
+    product.getProductInformation()
+}
+// console.log(_.shuffle([1,2,3]));
